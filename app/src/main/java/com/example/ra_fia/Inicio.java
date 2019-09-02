8f63b8be1325cf881f266f825beebea7bce233de
Wolf-JhEd <edwar.salas.987@gmail.com>
2019-09-02 02:22:40 -0500

    fondo

---
+++ app/src/main/java/com/example/ra_fia/Inicio.java
@@ -0,0 +1,15 @@
+package com.example.ra_fia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Inicio extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
--- app/src/main/java/com/example/ra_fia/MainActivity.java
+++ app/src/main/java/com/example/ra_fia/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.ra_fia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private  final int DURACION_SPLASH = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(MainActivity.this, Inicio.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
     }
 }
